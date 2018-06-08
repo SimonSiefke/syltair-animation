5366db233cf341de45b205c84c27d5b4488dcb9b
SimonSiefke <simon.siefke@gmail.com>
2018-06-08 15:02:05 +0200

    stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
+
+
+const path = require('path');
+
 module.exports = {
   entry: './src/index.ts',
-  output: {
-    filename: './bundle.js',
- },
-  resolve: {
-    // Add `.ts` and `.tsx` as a resolvable extension.
-    extensions: [".ts", ".tsx", ".js"]
-  },
   module: {
     rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: "ts-loader" }
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
     ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   }
-}
+};
